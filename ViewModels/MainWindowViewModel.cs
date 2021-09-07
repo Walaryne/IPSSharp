10f4761374178d288fb03253e90a9d718903f430
Walaryne <hazardoustheradioactive@gmail.com>
2021-09-06 22:21:13 -0400

    File pickers now work (more progress!)

--- ViewModels/MainWindowViewModel.cs
+++ ViewModels/MainWindowViewModel.cs
@@ -1,7 +1,6 @@
-﻿using System.Threading;
+﻿using System;
 using System.Threading.Tasks;
 using ReactiveUI;
-
 namespace IPSSharp.ViewModels
 {
     public class MainWindowViewModel : ViewModelBase
@@ -9,11 +8,34 @@ namespace IPSSharp.ViewModels
         public string Greeting => "IPSSharp v0.1.0";
 
         private string statusLineText;
+        
+        private string romFileText;
+        private string ipsFileText;
+        private string outputFileText;
+        
         private bool overwriteCheckboxValue;
         public string StatusLineText
         {
             get => statusLineText;
             set => this.RaiseAndSetIfChanged(ref statusLineText, value);
+        }
+
+        public string RomFileText
+        {
+            get => romFileText;
+            set => this.RaiseAndSetIfChanged(ref romFileText, value);
+        }
+        
+        public string IPSFileText
+        {
+            get => ipsFileText;
+            set => this.RaiseAndSetIfChanged(ref ipsFileText, value);
+        }
+        
+        public string OutputFileText
+        {
+            get => outputFileText;
+            set => this.RaiseAndSetIfChanged(ref outputFileText, value);
         }
 
         public bool OverwriteCheckboxValue
@@ -25,6 +47,10 @@ namespace IPSSharp.ViewModels
         public MainWindowViewModel()
         {
             statusLineText = "Waiting...";
+            
+            romFileText = string.Empty;
+            ipsFileText = string.Empty;
+            outputFileText = string.Empty;
         }
 
         public async Task PatchClicked()
@@ -32,8 +58,10 @@ namespace IPSSharp.ViewModels
             StatusLineText = "Patching...";
             await Task.Delay(5000);
             StatusLineText = "Patched!";
-        }
-        
 
+            await Console.Out.WriteLineAsync(romFileText);
+            await Console.Out.WriteLineAsync(ipsFileText);
+            await Console.Out.WriteLineAsync(outputFileText);
+        }
     }
 }
--- Views/MainWindow.axaml.cs
+++ Views/MainWindow.axaml.cs
@@ -1,11 +1,9 @@
-using Avalonia;
 using Avalonia.Controls;
 using Avalonia.Input;
 using Avalonia.Interactivity;
 using Avalonia.Markup.Xaml;
 using Avalonia.ReactiveUI;
 using IPSSharp.ViewModels;
-
 namespace IPSSharp.Views
 {
     public partial class MainWindow : ReactiveWindow<MainWindowViewModel>
@@ -30,6 +28,28 @@ namespace IPSSharp.Views
         private void InputElement_OnPointerPressed(object? sender, PointerPressedEventArgs e)
         {
             this.Get<Panel>("RootPanel").Focus();
+        }
+        private void RomFilePickerButton_OnClick(object? sender, RoutedEventArgs e)
+        {
+            GetFileFromDialog("RomFileText");
+        }
+        private void IPSFilePickerButton_OnClick(object? sender, RoutedEventArgs e)
+        {
+            GetFileFromDialog("IPSFileText");
+        }
+        private void OutputFilePickerButton_OnClick(object? sender, RoutedEventArgs e)
+        {
+            GetFileFromDialog("OutputFileText");
+        }
+
+        //Awaits a file dialog's return value and sets the Text value of TextBox with name "name"
+        private async void GetFileFromDialog(string name)
+        {
+            string[] filePaths = await new OpenFileDialog().ShowAsync(this);
+            if (filePaths != null)
+            {
+                this.FindControl<TextBox>(name).Text = filePaths[0];
+            }
         }
     }
 }
